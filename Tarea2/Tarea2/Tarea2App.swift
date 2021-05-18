816382879c584ab7eb1e211d7e1ae31146485968
Ulises <amao.apps@gmail.com>
2021-05-17 22:26:09 -0500

    Agregado la tarea2

---
+++ Tarea2/Tarea2/Tarea2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tarea2App.swift
+//  Tarea2
+//
+//  Created by Paku on 17/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Tarea2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
